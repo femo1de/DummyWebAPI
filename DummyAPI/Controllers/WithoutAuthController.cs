eb1b67e9bd54cfe7098d2f5e64ced3046b0a4f5c
Felix <femora@microsoft.com>
2022-02-14 16:16:35 +0000

    added swagger

--- DummyAPI/Controllers/WithoutAuthController.cs
+++ DummyAPI/Controllers/WithoutAuthController.cs
@@ -17,19 +17,19 @@ namespace DummyAPI.Controllers
             return "value";
         }
 
-        // POST api/values
-        public void Post([FromBody] string value)
-        {
-        }
+        //// POST api/values
+        //public void Post([FromBody] string value)
+        //{
+        //}
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
